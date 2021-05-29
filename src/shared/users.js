47b5d6976583c2aefd41cff4e09e24ab190f1993
Roz <rosalindavalbrecht@gmail.com>
2021-05-29 14:04:32 -0700

    added users.js

---
+++ src/shared/users.js
@@ -0,0 +1,163 @@
+export const USERS = [
+    {
+        id: 0,
+        firstName: "Commander",
+        lastName: "Shepard",
+        password: "1234",
+        img: '/img/users/Shepard.jpg'
+    },
+    {
+        id: 1,
+        firstName: "Kaidan",
+        lastName: "Alenko",
+        password: "1234",
+        img: 'img/users/Kaidan.jpg'
+    },
+    {
+        id: 2,
+        firstName: "Tali'Zorah",
+        lastName: "vas Normandy",
+        password: "1234",
+        img: 'img/users/Tali.jpg'
+    },
+    {
+        id: 3,
+        firstName: "Liara",
+        lastName: "T'soni",
+        password: "1234",
+        img: 'img/users/Liara.jpg'
+    },
+    {
+        id: 4,
+        firstName: "Garrus",
+        lastName: "Vakarian",
+        password: "1234",
+        img: 'img/users/Garrus.png'
+    },
+    {
+        id: 5,
+        firstName: "Ashley",
+        lastName: "Williams",
+        password: "1234",
+        img: 'img/users/Ashley.jpg'
+    },
+    {
+        id: 6,
+        firstName: "Urdnot",
+        lastName: "Wrex",
+        password: "1234",
+        img: 'img/users/Wrex.jpg'
+    },
+    {
+        id: 7,
+        firstName: "Kasumi",
+        lastName: "Goto",
+        password: "1234",
+        img: 'img/users/Kasumi.jpg'
+    },
+    {
+        id: 8,
+        firstName: "Grunt",
+        lastName: "",
+        password: "1234",
+        img: 'img/users/Grunt.png'
+    },
+    {
+        id: 9,
+        firstName: "Thane",
+        lastName: "Krios",
+        password: "1234",
+        img: 'img/users/Thane.png'
+    },
+    {
+        id: 10,
+        firstName: "Jack",
+        lastName: "",
+        password: "1234",
+        img: 'img/users/Jack.jpg'
+    },
+    {
+        id: 11,
+        firstName: "Miranda",
+        lastName: "Lawson",
+        password: "1234",
+        img: 'img/users/Miranda.jpg'
+    },
+    {
+        id: 12,
+        firstName: "Legion",
+        lastName: "",
+        password: "1234",
+        img: 'img/users/Legion.png'
+    },
+    {
+        id: 13,
+        firstName: "Zaeed",
+        lastName: "Massani",
+        password: "1234",
+        img: 'img/users/Zaeed.jpg'
+    },
+    {
+        id: 14,
+        firstName: "Samara",
+        lastName: "",
+        password: "1234",
+        img: 'img/users/Samara.png'
+    },
+    {
+        id: 15,
+        firstName: "Morinth",
+        lastName: "",
+        password: "1234",
+        img: 'img/users/Morinth.jpg'
+    },
+    {
+        id: 16,
+        firstName: "Mordin",
+        lastName: "Solus",
+        password: "1234",
+        img: 'img/users/Mordin.jpg'
+    },
+    {
+        id: 17,
+        firstName: "Jacob",
+        lastName: "Taylor",
+        password: "1234",
+        img: 'img/users/Jacob.jpg'
+    },
+    {
+        id: 18,
+        firstName: "Javik",
+        lastName: "",
+        password: "1234",
+        img: 'img/users/Javik.jpg'
+    },
+    {
+        id: 19,
+        firstName: "James",
+        lastName: "Vega",
+        password: "1234",
+        img: 'img/users/James.jpg'
+    },
+    {
+        id: 20,
+        firstName: "Edi",
+        lastName: "",
+        password: "1234",
+        img: 'img/users/Edi.jpg'
+    },
+    {
+        id: 21,
+        firstName: "Illusive",
+        lastName: "Man",
+        password: "1234",
+        img: 'img/users/illusiveMan.jpg'
+    },
+    {
+        id: 22,
+        firstName: "Conrad",
+        lastName: "Verner",
+        password: "1234",
+        img: 'img/users/ConradVerner.png'
+    },
+]
