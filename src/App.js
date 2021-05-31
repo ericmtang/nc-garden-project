07a8d870dcee68b0b3a5cf8c4332b4ac10d05ac0
ericmtang <80090030+ericmtang@users.noreply.github.com>
2021-05-31 16:35:06 -0700

    classifieds post modal changes

--- src/App.js
+++ src/App.js
@@ -9,24 +9,29 @@ import Classifieds from "./components/Classifieds";
 import Recipes from "./components/Recipes";
 import PlantDB from "./components/PlantDB";
 import "./App.css";
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 function App() {
   return (
     <div>
       <HashRouter>
         <Header />
-        <div className="container mt-2" style={{ marginTop: 40 }}>
-          <Switch>
-            <Route exact path="/" render={(props) => <Home />} />
-            <Route path="/blog" render={(props) => <Blog />} />
-            <Route path="/store/detail" render={(props) => <StoreDetail />} />
-            <Route path="/store" render={(props) => <Store />} />
-            <Route path="/classifieds" render={(props) => <Classifieds />} />
-            <Route path="/recipes" render={(props) => <Recipes />} />
-            <Route path="/plantdb" render={(props) => <PlantDB />} />
-            <Route path="/home" render={(props) => <Home />} />
-          </Switch>
-        </div>
+        <TransitionGroup>
+          <CSSTransition in={true} timeout={200} classNames="page">
+            {/*<div className="container mt-2" style={{ marginTop: 40 }}>*/}
+            <Switch>
+              <Route exact path="/" render={(props) => <Home />} />
+              <Route path="/blog" render={(props) => <Blog />} />
+              <Route path="/store/detail" render={(props) => <StoreDetail />} />
+              <Route path="/store" render={(props) => <Store />} />
+              <Route path="/classifieds" render={(props) => <Classifieds />} />
+              <Route path="/recipes" render={(props) => <Recipes />} />
+              <Route path="/plantdb" render={(props) => <PlantDB />} />
+              <Route path="/home" render={(props) => <Home />} />
+            </Switch>
+            {/*</div>*/}
+          </CSSTransition>
+        </TransitionGroup>
       </HashRouter>
     </div>
   );
