7385bba709f35820f785d67211eb6e87d58a1358
Roz <rosalindavalbrecht@gmail.com>
2021-06-12 13:42:10 -0700

    Updated the comments - tedious work!

--- src/shared/blogComments.js
+++ src/shared/blogComments.js
@@ -12,9 +12,318 @@ export const BLOGCOMMENTS = [
     {
         commentId: 1,
         postId: 0,
-        author: USERS[3],
+        author: USERS[14],
         date: "2019-08-04T20:11Z",
         text: "Astonishment rogue hundreds of thousands the only home we've ever known made in the interiors of collapsing stars colonies? Extraplanetary at the edge of forever two ghostly white figures in coveralls and helmets are softly dancing from which we spring cosmic fugue dream of the mind's eye? Encyclopaedia galactica courage of our questions not a sunrise but a galaxyrise something incredible is waiting to be known the sky calls to us network of wormholes and billions upon billions upon billions upon billions upon billions upon billions upon billions."
     },
+    {
+        commentId: 2,
+        postId: 0,
+        author: USERS[1],
+        date: "2019-08-04T20:11Z",
+        text: `Found them? In Mercia?! The coconut's tropical!
+        Bloody Peasant! Burn her! How do you know she is a witch? Knights of Ni, we are but simple travelers who seek the enchanter who lives beyond these woods. Shut up! Will you shut up?!
+        
+        We shall say 'Ni' again to you, if you do not appease us. You can't expect to wield supreme power just 'cause some watery tart threw a sword at you! She looks like one. Well, what do you want? Listen. Strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony.`
+    },
+    {
+        commentId: 3,
+        postId: 0,
+        author: USERS[12],
+        date: "2019-08-04T20:11Z",
+        text: `Well, what do you want?
+        The Lady of the Lake, her arm clad in the purest shimmering samite, held aloft Excalibur from the bosom of the water, signifying by divine providence that I, Arthur, was to carry Excalibur. That is why I am your king. Be quiet!
+        
+        And this isn't my nose. This is a false one.
+        What a strange person.
+        Now, look here, my good man.`
+    },
+    {
+        commentId: 4,
+        postId: 1,
+        author: USERS[20],
+        date: "2019-08-04T20:11Z",
+        text: `I have to push the pram a lot.
+        What a strange person. Ah, now we see the violence inherent in the system! You don't frighten us, English pig-dogs! Go and boil your bottoms, sons of a silly person! I blow my nose at you, so-called Ah-thoor Keeng, you and all your silly English K-n-n-n-n-n-n-n-niggits!
+        
+        Camelot!
+        The nose?
+        Why do you think that she is a witch?
+        How do you know she is a witch? We found them. Look, my liege! I have to push the pram a lot.
+        
+        Oh! Come and see the violence inherent in the system! Help, help, I'm being repressed! Did you dress her up like this? The Lady of the Lake, her arm clad in the purest shimmering samite, held aloft Excalibur from the bosom of the water, signifying by divine providence that I, Arthur, was to carry Excalibur. That is why I am your king.`
+    },
+    {
+        commentId: 5,
+        postId: 1,
+        author: USERS[5],
+        date: "2019-07-04T20:11Z",
+        text: `Now, look here, my good man. Listen. Strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony.
 
+        A newt?`
+    },
+    {
+        commentId: 4,
+        postId: 1,
+        author: USERS[3],
+        date: "2019-08-04T20:11Z",
+        text: `On second thoughts, let's not go there. It is a silly place. No, no, no! Yes, yes. A bit. But she's got a wart. The Knights Who Say Ni demand a sacrifice! What do you mean? Shut up! Will you shut up?!
+
+        How do you know she is a witch? The Knights Who Say Ni demand a sacrifice! Shut up! Will you shut up?! No, no, no! Yes, yes. A bit. But she's got a wart. Be quiet! Burn her anyway!`
+    },
+    {
+        commentId: 6,
+        postId: 2,
+        author: USERS[2],
+        date: "2019-08-03T20:11Z",
+        text:`Burn her! But you are dressed as one… Did you dress her up like this? Bloody Peasant! How do you know she is a witch?
+
+        It's only a model. Knights of Ni, we are but simple travelers who seek the enchanter who lives beyond these woods. You don't vote for kings. Well, we did do the nose.` 
+    },    
+    {
+        commentId: 7,
+        postId: 3,
+        author: USERS[0],
+        date: "2020-06-04T20:11Z",
+        text: `I am your king. And the hat. She's a witch! Listen. Strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony.
+
+        Well, how'd you become king, then? Now, look here, my good man. Did you dress her up like this? I don't want to talk to you no more, you empty-headed animal food trough water! I fart in your general direction! Your mother was a hamster and your father smelt of elderberries! Now leave before I am forced to taunt you a second time!`
+    },
+    {
+        commentId: 8,
+        postId: 5,
+        author: USERS[6],
+        date: "2020-08-04T20:11Z",
+        text: `Aaah! Natural light! Get it off me! Get it off me!
+        Marge, it takes two to lie. One to lie and one to listen. Mrs. Krabappel and Principal Skinner were in the closet making babies and I saw one of the babies and then the baby looked at me. I've done everything the Bible says — even the stuff that contradicts the other stuff!
+        
+        A lifetime of working with nuclear power has left me with a healthy green glow…and left me as impotent as a Nevada boxing commissioner. But, Aquaman, you cannot marry a woman without gills. You're from two different worlds… Oh, I've wasted my life.`
+    },
+    {
+        commentId: 9,
+        postId: 6,
+        author: USERS[6],
+        date: "2019-08-04T20:11Z",
+        text: `Ahoy hoy?
+        You don't win friends with salad. Here's to alcohol, the cause of — and solution to — all life's problems. Well, he's kind of had it in for me ever since I accidentally ran over his dog. Actually, replace "accidentally" with "repeatedly" and replace "dog" with "son."
+        
+        I didn't get rich by signing checks.
+        "Thank the Lord"? That sounded like a prayer. A prayer in a public school. God has no place within these walls, just like facts don't have a place within an organized religion.
+        Well, he's kind of had it in for me ever since I accidentally ran over his dog. Actually, replace "accidentally" with "repeatedly" and replace "dog" with "son."`
+    },
+    {
+        commentId: 10,
+        postId: 7,
+        author: USERS[19],
+        date: "2021-03-04T20:11Z",
+        text: `Jesus must be spinning in his grave!
+        …And the fluffy kitten played with that ball of string all through the night. On a lighter note, a Kwik-E-Mart clerk was brutally murdered last night. Bart, with $10,000 we'd be millionaires! We could buy all kinds of useful things like…love!
+        
+        Old people don't need companionship. They need to be isolated and studied so it can be determined what nutrients they have that might be extracted for our personal use.
+        I hope I didn't brain my damage.
+        Beer. Now there's a temporary solution.`
+    },
+    {
+        commentId: 11,
+        postId: 8,
+        author: USERS[21],
+        date: "2019-08-04T20:11Z",
+        text: `I didn't think it was physically possible, but this both sucks *and* blows. Lisa, vampires are make-believe, like elves, gremlins, and Eskimos. Our differences are only skin deep, but our sames go down to the bone.
+
+        Can't you people take the law into your own hands? I mean, we can't be policing the entire city! Stan Lee never left. I'm afraid his mind is no longer in mint condition. Your questions have become more redundant and annoying than the last three "Highlander" movies.`
+    },
+    {
+        commentId: 12,
+        postId: 8,
+        author: USERS[9],
+        date: "2019-08-04T20:11Z",
+        text: `Oh, so they have Internet on computers now! Marge, just about everything's a sin. Y'ever sat down and read this thing? Technically we're not supposed to go to the bathroom. I've done everything the Bible says — even the stuff that contradicts the other stuff!`
+    },
+    {
+        commentId: 13,
+        postId: 9,
+        author: USERS[17],
+        date: "2019-08-04T20:11Z",
+        text: `But, Aquaman, you cannot marry a woman without gills. You're from two different worlds… Oh, I've wasted my life. Oh, everything looks bad if you remember it. When I held that gun in my hand, I felt a surge of power…like God must feel when he's holding a gun.
+
+        Homer no function beer well without. I hope this has taught you kids a lesson: kids never learn. Uh, no, you got the wrong number. This is 9-1…2. I hope this has taught you kids a lesson: kids never learn.`
+    },
+    {
+        commentId: 14,
+        postId: 10,
+        author: USERS[15],
+        date: "2019-08-04T20:11Z",
+        text: `Homer no function beer well without. I hope this has taught you kids a lesson: kids never learn. Uh, no, you got the wrong number. This is 9-1…2. I hope this has taught you kids a lesson: kids never learn.
+
+        Look out, Itchy! He's Irish! This is the greatest case of false advertising I've seen since I sued the movie "The Never Ending Story." Kids, you tried your best and you failed miserably. The lesson is, never try.`
+    },
+    {
+        commentId: 15,
+        postId: 11,
+        author: USERS[19],
+        date: "2019-08-04T20:11Z",
+        text: `What do you mean? He hasn't got shit all over him. Shut up! Burn her!
+
+        Burn her! What a strange person. I have to push the pram a lot. And the hat. She's a witch! Did you dress her up like this?
+        
+        What do you mean? I dunno. Must be a king. No, no, no! Yes, yes. A bit. But she's got a wart. Well, I didn't vote for you. A newt?`
+    },
+    {
+        commentId: 16,
+        postId: 12,
+        author: USERS[4],
+        date: "2019-08-04T20:11Z",
+        text: `Inflammable means flammable? What a country. They only come out in the night. Or in this case, the day. They only come out in the night. Or in this case, the day. How is education supposed to make me feel smarter? Besides, every time I learn something new, it pushes some old stuff out of my brain. Remember when I took that home winemaking course, and I forgot how to drive?` 
+    },
+    {
+        commentId: 17,
+        postId: 12,
+        author: USERS[13],
+        date: "2019-08-04T20:11Z",
+        text: `I can't go to juvie. They use guys like me as currency. Marge, it takes two to lie. One to lie and one to listen. Shoplifting is a victimless crime. Like punching someone in the dark. Jesus must be spinning in his grave!`
+    },
+    {
+        commentId: 18,
+        postId: 13,
+        author: USERS[18],
+        date: "2019-08-04T20:11Z",
+        text: `Well, Mercia's a temperate zone! The nose? But you are dressed as one… Oh, ow!`
+    },
+    {
+        commentId: 19,
+        postId: 13,
+        author: USERS[11],
+        date: "2019-08-04T20:11Z",
+        text: `Dear Mr. President, There are too many states nowadays. Please, eliminate three. P.S. I am not a crackpot. Kids, you tried your best and you failed miserably. The lesson is, never try. When I held that gun in my hand, I felt a surge of power…like God must feel when he's holding a gun.`
+    },
+    {
+        commentId: 20,
+        postId: 14,
+        author: USERS[12],
+        date: "2019-08-04T20:11Z",
+        text: `What a strange person. Shh! Knights, I bid you welcome to your new home. Let us ride to Camelot! She looks like one. She looks like one.`
+    },
+    {
+        commentId: 21,
+        postId: 15,
+        author: USERS[0],
+        date: "2019-08-04T20:11Z",
+        text: `I'm half machine. I'm a monster. Get me a vodka rocks. And a piece of toast. Now, when you do this without getting punched in the chest, you'll have more fun. It's called 'taking advantage.' It's what gets you ahead in life.`
+    },
+    {
+        commentId: 22,
+        postId: 16,
+        author: USERS[1],
+        date: "2019-08-04T20:11Z",
+        text: `There's so many poorly chosen words in that sentence. I don't understand the question, and I won't respond to it. I'm half machine. I'm a monster. No, I did not kill Kitty. However, I am going to oblige and answer the nice officer's questions because I am an honest man with no secrets to hide`
+    },
+    {
+        commentId: 23,
+        postId: 16,
+        author: USERS[2],
+        date: "2019-08-04T20:11Z",
+        text: `Did you enjoy your meal, Mom? You drank it fast enough. Not tricks, Michael, illusions. That's why you always leave a note! I'm a monster.`
+    },
+    {
+        commentId: 24,
+        postId: 17,
+        author: USERS[18],
+        date: "2019-08-04T20:11Z",
+        text: `There's so many poorly chosen words in that sentence. I don't understand the question, and I won't respond to it. I'm half machine. I'm a monster. No, I did not kill Kitty. However, I am going to oblige and answer the nice officer's questions because I am an honest man with no secrets to hide.`
+    },
+    {
+        commentId: 25,
+        postId: 18,
+        author: USERS[19],
+        date: "2019-08-04T20:11Z",
+        text: `I hear the jury's still out on science. Not tricks, Michael, illusions. I care deeply for nature. That's why you always leave a note!` 
+    },    
+    {
+        commentId: 26,
+        postId: 19,
+        author: USERS[15],
+        date: "2021-08-12T20:11Z",
+        text: `Obi-Wan is here. The Force is with him.
+        I suggest you try it again, Luke. This time, let go your conscious self and act on instinct. Hey, Luke! May the Force be with you. I suggest you try it again, Luke. This time, let go your conscious self and act on instinct.`
+    },
+    {
+        commentId: 27,
+        postId: 20,
+        author: USERS[11],
+        date: "2020-11-04T20:11Z",
+        text: `No! Alderaan is peaceful. We have no weapons. You can't possibly…`
+    },
+    {
+        commentId: 28,
+        postId: 20,
+        author: USERS[0],
+        date: "2019-08-04T20:11Z",
+        text: `What?! I want to come with you to Alderaan. There's nothing for me here now. I want to learn the ways of the Force and be a Jedi, like my father before me. I have traced the Rebel spies to her. Now she is my only link to finding their secret base.
+
+        Partially, but it also obeys your commands.
+        I need your help, Luke. She needs your help. I'm getting too old for this sort of thing.
+        I have traced the Rebel spies to her. Now she is my only link to finding their secret base.`
+    },
+    {
+        commentId: 29,
+        postId: 17,
+        author: USERS[1],
+        date: "2019-08-04T20:11Z",
+        text: `Oh God, my uncle. How am I ever gonna explain this?
+        I want to come with you to Alderaan. There's nothing for me here now. I want to learn the ways of the Force and be a Jedi, like my father before me. The more you tighten your grip, Tarkin, the more star systems will slip through your fingers.` 
+    },
+    {
+        commentId: 30,
+        postId: 16,
+        author: USERS[2],
+        date: "2020-01-02T20:11Z",
+        text: `I want to come with you to Alderaan. There's nothing for me here now. I want to learn the ways of the Force and be a Jedi, like my father before me.
+        Don't be too proud of this technological terror you've constructed. The ability to destroy a planet is insignificant next to the power of the Force.`
+    },
+    {
+        commentId: 31,
+        postId: 15,
+        author: USERS[3],
+        date: "2020-09-04T20:11Z",
+        text: `Still, she's got a lot of spirit. I don't know, what do you think? I don't know what you're talking about. I am a member of the Imperial Senate on a diplomatic mission to Alderaan-- I have traced the Rebel spies to her. Now she is my only link to finding their secret base.`
+    },
+    {
+        commentId: 32,
+        postId: 14,
+        author: USERS[3],
+        date: "2019-08-04T20:11Z",
+        text: `Don't be too proud of this technological terror you've constructed. The ability to destroy a planet is insignificant next to the power of the Force. But with the blast shield down, I can't even see! How am I supposed to fight?`
+    },
+    {
+        commentId: 33,
+        postId: 17,
+        author: USERS[4],
+        date: "2021-06-04T20:11Z",
+        text: `The Force is strong with this one. I have you now. Kid, I've flown from one side of this galaxy to the other. I've seen a lot of strange stuff, but I've never seen anything to make me believe there's one all-powerful Force controlling everything. There's no mystical energy field that controls my destiny. It's all a lot of simple tricks and nonsense.
+
+        A tremor in the Force. The last time I felt it was in the presence of my old master. I can't get involved! I've got work to do! It's not that I like the Empire, I hate it, but there's nothing I can do about it right now. It's such a long way from here.`
+    },
+    {
+        commentId: 33,
+        postId: 13,
+        author: USERS[5],
+        date: "2019-08-04T20:11Z",
+        text: `You're all clear, kid. Let's blow this thing and go home! I call it luck. No! Alderaan is peaceful. We have no weapons. You can't possibly… Partially, but it also obeys your commands.`
+    },
+    {
+        commentId: 34,
+        postId: 12,
+        author: USERS[6],
+        date: "2019-08-04T20:11Z",
+        text: `No! Alderaan is peaceful. We have no weapons. You can't possibly… I need your help, Luke. She needs your help. I'm getting too old for this sort of thing. Still, she's got a lot of spirit. I don't know, what do you think?
+
+        I suggest you try it again, Luke. This time, let go your conscious self and act on instinct. A tremor in the Force. The last time I felt it was in the presence of my old master. I find your lack of faith disturbing.`
+    },
+    {
+        commentId: 35,
+        postId: 17,
+        author: USERS[7],
+        date: "2019-08-04T20:11Z",
+        text: `On second thoughts, let's not go there. It is a silly place. But you are dressed as one… The Lady of the Lake, her arm clad in the purest shimmering samite, held aloft Excalibur from the bosom of the water, signifying by divine providence that I, Arthur, was to carry Excalibur. That is why I am your king.`
+    },
 ]
